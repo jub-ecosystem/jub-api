db9d104cf298a286da0ef3ea04a3150144e2ed36
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-03-30 16:56:42 -0600

    fix deployment issues

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 readonly env_file=${1:-".env.dev"}
 readonly build=${2:-1}
-docker volume create xolo-db > /dev/null 2>&1 || true
+# docker volume create xolo-db > /dev/null 2>&1 || true
 docker network create jub > /dev/null 2>&1 || true
 
 echo "Using environment file: $env_file"
@@ -15,5 +15,5 @@ else
     docker compose -p jub --env-file "$env_file" -f docker-compose.yml up -d
 fi
 
-docker compose -p jub --env-file "$env_file" -f xolo.yml down
-docker compose -p jub --env-file "$env_file" -f xolo.yml up -d
+# docker compose -p jub --env-file "$env_file" -f xolo.yml down
+# docker compose -p jub --env-file "$env_file" -f xolo.yml up -d
